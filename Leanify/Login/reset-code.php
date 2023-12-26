0a9a7107d6949f30887086eee33d67eff942ecad
daalsultan <148685708+daalsultan@users.noreply.github.com>
2023-12-26 21:59:43 +0300

    Add files via upload

---
+++ Leanify/Login/reset-code.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Code Verification</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="reset-code.php" method="POST" autocomplete="off">
+                    <h2 class="text-center">Code Verification</h2>
+                    <div class="form-group">
+                        <input class="form-control" type="number" name="otp" placeholder="Enter code" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="check-reset-otp" value="Submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>

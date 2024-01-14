c34b13d0c52223df3f8ee594e85f5ea23e743098
daalsultan <148685708+daalsultan@users.noreply.github.com>
2024-01-14 20:20:16 +0300

    Add files via upload

---
+++ Leanify/login/new-password.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create a New Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="index.php" method="POST" autocomplete="off">
+                    <h2 class="text-center">New Password</h2>
+                    
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="password" placeholder="Create new password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="cpassword" placeholder="Confirm your password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="change-password" value="Change">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
